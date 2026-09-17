c1a9b8478fdbd32763b72e840e1163aac741f107
ARNAB PAUL <arnabpaul2k20@gmail.com>
2026-09-17 21:58:19 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<string , vector<string>>groups;
+        for(string &str : strs){
+            string key = str;
+            sort(key.begin(),key.end());
+            groups[key].push_back(str);
+        }
+        for(auto& [key,group]: groups){
+            ans.push_back(group);
+        }
+        return ans;
+    }
+};
